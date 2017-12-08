ce6feae6da758a5b416ffebd12bed7835a3626a7
Timothy Pressey <timothy.pressey@gmail.com>
2017-12-08 21:13:56 +0000

    added delete button

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,13 +3,26 @@ class RatingsController < ApplicationController
     @product = Product.find(params[:product_id])
     @rating = @product.ratings.create(rating_params)
     @rating.user = current_user
-    @rating.save
+    if (@rating.save)
+      redirect_to product_path(@product)
+    else
+      render product_path(@product)
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @rating = @product.ratings.find(params[:id])
+    @rating.destroy
     redirect_to product_path(@product)
+  end
+
+  before_filter do
+    throw :error unless current_user
   end
 
   private
   def rating_params
-    r = params.require(:rating).permit(:rating, :description)
-    r.user = current_user
+    params.require(:rating).permit(:rating, :description)
   end
 end
