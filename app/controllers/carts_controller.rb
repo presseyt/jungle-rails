77edc6782acf5f718752ec5c65d240fc9441a2b4
Timothy Pressey <timothy.pressey@gmail.com>
2017-12-08 17:47:43 +0000

    created migration and model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 
   def show
+    render 'empty_cart' if cart.keys.length == 0
   end
 
   def add_item
---
+++ app/models/rating.rb
@@ -0,0 +1,8 @@
+class Rating < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+
+  # validates :user, presence: true
+  # validates :product, presence: true
+  # validates :rating, presence: true
+end
